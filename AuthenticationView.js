5ab4a6fceaa7340c8b9963fedab5cb9adf1eca49
Martin <martin.brude@gmail.com>
2018-02-21 16:32:32 -0300

    Navigation bar is gone!

---
+++ AuthenticationView.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {Platform, StyleSheet, Text, Image, View} from 'react-native';
+
+export default class AuthenticationView extends Component {
+
+  render() {
+    return (
+      <View style={styles.container}>
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+  }
+});
---
+++ HomeScreen.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {Platform, StyleSheet, Text, Image, View, StatusBar, NavigatorIOS} from 'react-native';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class HomeScreen extends Component {
+  
+  componentDidMount(){
+    const { navigate } = this.props.navigation;
+    this.timeoutHandle = setTimeout(()=>{
+      navigate('AuthenticationView')
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Image 
+          style={{width: 200, height: 200}}
+          source={require('./logo.png')}/>
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
