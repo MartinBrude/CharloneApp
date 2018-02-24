030de7709d1063322b6a0c8657c026b0a757052d
Martin <martin.brude@gmail.com>
2018-02-24 15:31:50 -0300

    List view prototype

--- App.js
+++ App.js
@@ -8,11 +8,13 @@ import React, { Component } from 'react';
 import {Platform, StyleSheet, Text, Image, View} from 'react-native';
 import {StackNavigator} from 'react-navigation';
 import HomeScreen from './HomeScreen.js';
+import ListView from './ListView.js';
 import AuthenticationView from './AuthenticationView.js';
 
 export default App = StackNavigator({
   Home: { screen: HomeScreen },
-  AuthenticationView: { screen: AuthenticationView}
+  AuthenticationView: { screen: AuthenticationView},
+  ListView: {screen : ListView},
 }, {
   headerMode: 'none',
   navigationOptions: { 
--- AuthenticationView.js
+++ AuthenticationView.js
@@ -9,6 +9,14 @@ import {Platform, StyleSheet, Text, Image, View} from 'react-native';
 
 export default class AuthenticationView extends Component {
 
+  componentDidMount(){
+    const { navigate } = this.props.navigation;
+    this.timeoutHandle = setTimeout(()=>{
+      navigate('ListView')
+    }, 1000);
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
