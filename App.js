7a3aedfd37216087d80cd70a0e8222e39d875c67
Martin <martin.brude@gmail.com>
2018-03-02 13:32:38 -0300

    Remove commented code

--- App.js
+++ App.js
@@ -1,28 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-// import React, { Component } from 'react';
-// import {Platform, StyleSheet, Text, Image, View} from 'react-native';
-// import {StackNavigator} from 'react-navigation';
-// import HomeScreen from './HomeScreen.js';
-// import ListView from './ListView.js';
-// import AuthenticationView from './AuthenticationView.js';
-
-// export default App = StackNavigator({
-//   Home: { screen: HomeScreen },
-//   AuthenticationView: { screen: AuthenticationView},
-//   ListView: {screen : ListView},
-// }, {
-//   headerMode: 'none',
-//   navigationOptions: { 
-//     headerVisible: false
-//   }
-// });
-
-
 import React, { Component } from 'react';
 import { Tabs } from './router';
 
