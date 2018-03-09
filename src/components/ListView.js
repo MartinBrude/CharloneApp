1533922fd8ec51542a82de77c567d729b285ef7c
Martin <martin.brude@gmail.com>
2018-03-09 17:37:09 -0300

    Add heart icon to the list view

--- src/components/ListView.js
+++ src/components/ListView.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, SectionList, Text, Platform, Alert, Image } from 'react-native';
 import ListViewStyles from './ListViewStyles'
+import Icon from 'react-native-vector-icons/Ionicons';
+
 export default class ListView extends Component {
 
   render() { 
@@ -9,12 +11,15 @@ export default class ListView extends Component {
       <View style={{ marginTop : (Platform.OS) == 'ios' ? 10 : 0 }} >
       <SectionList
           sections={this.props.lists}
-
           renderSectionHeader={ ({section}) => <Text style={styles.SectionHeaderStyle}> { section.title } </Text> }
           renderItem={ ({item}) => 
           <View style={styles.ItemStyle}>
             <Image style={{width: 127, height: 150}} source={item.photo}/>
             <View style={styles.ItemContainerStyle}>
+                <View style={styles.ItemHeartIconStyle}>
+                  <Icon name="ios-heart-outline" size={25} color="#4F8EF7" /> 
+                </View>
+
               <Text style={styles.ItemTitleStyle}> { item.name } </Text>
               <Text style={styles.ItemDescriptionStyle}>
                 { item.description }
