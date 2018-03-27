bb614909f2715d80311121e98a1fb7ad3098b6f2
Martin <martin.brude@gmail.com>
2018-03-27 13:38:53 -0300

    scan qr

--- android/app/src/main/java/com/charloneapp/MainApplication.java
+++ android/app/src/main/java/com/charloneapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.charloneapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import org.reactnative.camera.RNCameraPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCameraPackage(),
             new VectorIconsPackage(),
             new SplashScreenReactPackage(),
             new VectorIconsPackage()
--- src/screens/ProfileView.js
+++ src/screens/ProfileView.js
@@ -1,11 +1,35 @@
+'use strict';
+
 import React, { Component } from 'react'
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Camera from 'react-native-camera'
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 export default class ProfileView extends Component {
 
-  render() { 
+  constructor(props) {
+    super(props);
+    this.state = {
+      qrcode: ''
+    }
+  }
+
+  qrDetected(e) {
+    this.setState({
+      qrcode: e.data
+    })
+  }
+
+  render() {
     return (
-      <View></View>
+      <View>
+        <View>
+          <QRCodeScanner onRead={this.qrDetected.bind(this)} />
+        </View>
+        <View>
+          <Text>{this.state.qrcode}</Text>
+        </View>
+      </View>
     )
   }
 }
